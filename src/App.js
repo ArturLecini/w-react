7c1e10ef34e8b2757e2ef76992f0d5b2bbd38273
ArturLecini <artur.lecini98@gmail.com>
2020-11-10 19:25:26 +0100

    website

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import {BrowserRouter as Router,Switch,Route }from 'react-router-dom'
-import Home from './components/pages/Home';
-import Products from './components/pages/Products';
-import Services from './components/pages/Services';
-import Footer from './components/Footer';
-import SignUp from './components/pages/SignUp';
+import SignUp from "./components/pages/SignUp";
+import Services from "./components/pages/Services";
+import Products from "./components/pages/Products";
+import Home from "./components/pages/Home";
+import Footer from "./components/Footer";
 function App() {
-  return (
-  <>
-  <Router>
-    <Navbar/> 
-    <Switch>
-      <Route path="/" exact component={Home}/>
-      <Route path="/products" component={Products}/>
-      <Route path="/services" component={Services}/>
-      <Route path="/sign-up" component={SignUp}/>
-    </Switch>
-    <Footer />
-
-    </Router>
-      
-
-     
-    </>
+  return ( <>
+    <Router>
+      <Navbar/> 
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/products" component={Products}/>
+        <Route path="/services" component={Services}/>
+        <Route path="/sign-up" component={SignUp}/>
+      </Switch>
+      <Footer />
+  
+      </Router>
+        
+  
+       
+      </>
   );
 }
 
